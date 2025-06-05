55d27c65a847db6a03f74b700b66c56146768815
jak <jackx.crane@hotmail.com>
2025-06-05 20:04:20 +0100

    safety push

--- Research_Files/June/sand_results_52.py
+++ Research_Files/June/sand_results_52.py
@@ -2,8 +2,8 @@
 # import pandas as pd
 
 # # Read the CSV files
-# input_file_1 = 'Research_Files/June/5_bar_sand/1_kermit106.csv'  # 0 2 7
-# input_file_2 = 'Research_Files/June/5_bar_sand/1_kermit108.csv'  # 0 1 7
+# input_file_1 = 'Research_Files/June/5_bar_sand/2_kermit106.csv'  # 0 2 7
+# input_file_2 = 'Research_Files/June/5_bar_sand/2_kermit108.csv'  # 0 1 7
 
 # # Load the data into a DataFrame
 # df_1 = pd.read_csv(input_file_1)
@@ -14,15 +14,15 @@
 # df_2 = df_2[['BackendTime', 'ch0sens', 'ch1sens', 'ch7sens']]
 
 # # Rescale BackendTime to seconds and set the first value to 0
-# df_1['BackendTime'] = (df_1['BackendTime'] - 1748806421850) / 1e3
-# df_2['BackendTime'] = (df_2['BackendTime'] - 1748806421850) / 1e3
+# df_1['BackendTime'] = (df_1['BackendTime'] - 1748808369580) / 1e3
+# df_2['BackendTime'] = (df_2['BackendTime'] - 1748808369580) / 1e3
 
 # # Replace any values in 'ch1sens' column of df_2 above 3 with 0
 # # df_2['ch1sens'] = df_2['ch1sens'].apply(lambda x: None if x > 3 else x)
 # # df_2['ch7sens'] = df_2['ch7sens'].apply(lambda x: None if x > 3 else x)
 
-# df_1 = df_1[(df_1['BackendTime'] >= 86) & (df_1['BackendTime'] <= 133)]
-# df_2 = df_2[(df_2['BackendTime'] >= 86) & (df_2['BackendTime'] <= 133)]
+# df_1 = df_1[(df_1['BackendTime'] >= 33) & (df_1['BackendTime'] <= 82)]
+# df_2 = df_2[(df_2['BackendTime'] >= 33) & (df_2['BackendTime'] <= 82)]
 # # df_2['ch7sens'] = df_2['ch7sens'].apply(lambda x: None if x is not None and x < 0 else x)
 
 
@@ -46,8 +46,8 @@
 # # Plot the values from df_2 against BackendTime
 # # plt.plot(df_2['BackendTime'], df_2['ch0sens'], label='Pre-choke PT', linestyle='--')
 # # plt.plot(df_2['BackendTime'], df_2['ch1sens'], label='Post-choke PT', linestyle='--')
-# plt.axvline(x=91.9, color='green', linestyle='--')
-# plt.axvline(x=126.7, color='green', linestyle='--')
+# plt.axvline(x=39.5, color='green', linestyle='--')
+# plt.axvline(x=74.9, color='green', linestyle='--')
 # # Remove all values before 8 seconds
 # # Add labels and title
 # plt.xlabel('Experiment Time (s)')
@@ -55,8 +55,7 @@
 # plt.ylim(0.1, 2.1)
 # plt.legend()
 # # Show the plot
-# # plt.show()
-# plt.savefig('./latex/report_assets/51_raw_mass.png')
+# plt.savefig('./latex/report_assets/52_raw_mass.png')
 # df_1['ch2plus7'] = df_1['ch2sens'] + df_1['ch7sens']
 # # Save cleaned DataFrames to new CSV files
 # df_1.to_csv(input_file_1.replace('.csv', '_clean.csv'), index=False)
@@ -67,8 +66,8 @@
 # import pandas as pd
 
 # # Read the CSV files
-# input_file_1 = 'Research_Files/June/5_bar_sand/1_kermit106_clean.csv'  # 0 2 7
-# input_file_2 = 'Research_Files/June/5_bar_sand/1_kermit108_clean.csv'  # 0 1 7
+# input_file_1 = 'Research_Files/June/5_bar_sand/2_kermit106_clean.csv'  # 0 2 7
+# input_file_2 = 'Research_Files/June/5_bar_sand/2_kermit108_clean.csv'  # 0 1 7
 
 # # Load the data into a DataFrame
 # df_1 = pd.read_csv(input_file_1)
@@ -94,8 +93,8 @@
 # # Plot the values from df_2 against BackendTime
 # # plt.plot(df_2['BackendTime'], df_2['ch0sens'], label='Pre-choke PT', linestyle='--')
 # # plt.plot(df_2['BackendTime'], df_2['ch1sens'], label='Post-choke PT', linestyle='--')
-# plt.axvline(x=91.9, color='green', linestyle='--')
-# plt.axvline(x=126.7, color='green', linestyle='--')
+# plt.axvline(x=39.5, color='green', linestyle='--')
+# plt.axvline(x=74.9, color='green', linestyle='--')
 # # Remove all values before 8 seconds
 # # Add labels and title
 # plt.xlabel('Experiment Time (s)')
@@ -103,15 +102,15 @@
 # plt.legend()
 
 # # # Show the plot
-# plt.savefig('./latex/report_assets/51_clean_mass.png')
+# plt.savefig('./latex/report_assets/52_clean_mass.png')
 
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Read the CSV files
-input_file_1 = 'Research_Files/June/5_bar_sand/1_kermit106_clean.csv'  # 0 2 7
-input_file_2 = 'Research_Files/June/5_bar_sand/1_kermit108_clean.csv'  # 0 1 7
+input_file_1 = 'Research_Files/June/5_bar_sand/2_kermit106_clean.csv'  # 0 2 7
+input_file_2 = 'Research_Files/June/5_bar_sand/2_kermit108_clean.csv'  # 0 1 7
 
 # Load the data into a DataFrame
 df_1 = pd.read_csv(input_file_1)
@@ -124,7 +123,7 @@ df_1['ch2plus7'] = -1*df_1['ch2plus7']
 df_1['m_flow'] = df_1['ch2plus7'].diff()
 df_2['m_flow'] = df_2['ch7sens'].diff()
 # Apply a rolling mean to smooth the curves
-window_size = 200  # Adjust as needed for smoothing
+window_size = 100  # Adjust as needed for smoothing
 df_1['smooth_combined'] = df_1['ch2plus7'].rolling(window=window_size, center=True, min_periods=1).mean()
 df_2['smooth_ch7sens'] = df_2['ch7sens'].rolling(window=window_size, center=True, min_periods=1).mean()
 
@@ -141,12 +140,12 @@ plt.rc('ytick', labelsize=24)   # Increase y-axis tick font size
 plt.plot(df_1['BackendTime'], df_1['m_flow_smooth'], label='Combined Tank', color='purple')
 plt.plot(df_2['BackendTime'], df_2['m_flow_smooth'], label='Cyclone Seperator', color='orange')
 
-plt.axvline(x=91.9, color='green', linestyle='--')
-plt.axvline(x=126.7, color='green', linestyle='--')
+plt.axvline(x=39.5, color='green', linestyle='--')
+plt.axvline(x=74.9, color='green', linestyle='--')
 
 plt.xlabel('Experiment Time (s)')
 plt.ylabel('Load Cell Values (kg)')
 plt.legend()
 
 # # Show the plot
-plt.savefig('./latex/report_assets/51_clean_flow_200.png')
+plt.savefig('./latex/report_assets/52_clean_flow_100.png')
